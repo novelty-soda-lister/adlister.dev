ebd7d2312250c4358a7833ee29ace5d4e842f64f
Craig Rupp <craigrupp@utexas.edu>
2016-08-10 15:47:52 -0500

    Navbar bare bones

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,1 +1,25 @@
 <!--partial view for navbar-->
+	<!-- NavBar-->
+	<nav class="navbar-default" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="/">Novely Sodas MF'er</a>
+			</div>
+	<!--CollapseNavBar-->
+			<div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="menuItem"><a href="/sodas">Sodas</a></li>
+					<li class="menuItem"><a href="/create">Create</a></li>
+					<li class="menuItem"><a href="/login">Login</a></li>
+					<li class="menuItem"><a href="/signup"></a>Signup</li>
+					<li class="menuItem"><a href="#home"></a>Logout</li>
+				</ul>
+			</div>	   
+		</div>
+	</nav>
